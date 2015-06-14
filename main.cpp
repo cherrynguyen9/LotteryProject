a9933fb64ded2f4e4a8c38e3b045e0ccaebe244b
lukas93 <wiederhold.lukas@web.de>
2015-06-14 14:50:32 +0200

    test commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Lukas Wiederhold
+ *
+ * Created on 14. Juni 2015, 14:50
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
